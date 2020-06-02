7296883584570dd1c6f5656a2cd92e56c569c50b
David Armendariz <darmendariz1998@outlook.com>
2020-06-02 15:07:08 -0500

    added dayjs to the dependencies

--- src/components/BiorhythmCard.js
+++ src/components/BiorhythmCard.js
@@ -5,12 +5,17 @@ import {
   IonCardTitle,
   IonCardContent,
 } from '@ionic/react';
+import dayjs from 'dayjs';
+
+function formatDate(isoString) {
+  return dayjs(isoString).format('D MMM YYYY');
+}
 
 const BiorhythmCard = ({ targetDate }) => {
   return (
     <IonCard className="ion-text-center">
       <IonCardHeader>
-        <IonCardTitle>{targetDate}</IonCardTitle>
+        <IonCardTitle>{formatDate(targetDate)}</IonCardTitle>
       </IonCardHeader>
       <IonCardContent>
         <p>Physical: 83%</p>
